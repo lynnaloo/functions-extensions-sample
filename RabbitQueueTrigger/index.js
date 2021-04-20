72365ccdee64b33e9a3b71d8ce969747fe59ee14
Linda Nichols <lynnaloo@gmail.com>
2021-04-20 14:35:17 -0400

    [config] fixed property names

--- RabbitQueueTrigger/index.js
+++ RabbitQueueTrigger/index.js
@@ -1,3 +1,3 @@
-module.exports = async function (context, queueItem) {​​
-    context.log('Linda JavaScript RabbitMQ trigger function processed work item', queueItem);
+module.exports = async function (context, item) {​​
+    context.log('Linda JavaScript RabbitMQ trigger function processed work item', item);
 }​​;
